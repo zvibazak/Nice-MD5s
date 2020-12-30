c6b80349bee54b7b3e2a6e253e2358d1586a13db
zvibazak <zvibazak@gmail.com>
2020-12-30 14:02:21 +0200

    Update find_md5.py

--- find_md5.py
+++ find_md5.py
@@ -3,8 +3,6 @@ import string
 from time import sleep
 from hashlib import md5
 
-nice_pattern = ['00000000000000000000000000000000', '11111111111111111111111111111111', '22222222222222222222222222222222', '33333333333333333333333333333333', '44444444444444444444444444444444', '55555555555555555555555555555555', '66666666666666666666666666666666', '77777777777777777777777777777777', '88888888888888888888888888888888', '99999999999999999999999999999999', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb', 'cccccccccccccccccccccccccccccccc', 'dddddddddddddddddddddddddddddddd', 'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 'ffffffffffffffffffffffffffffffff' ]
-
 N_nice_match = 6
 
 while(True):
@@ -12,10 +10,13 @@ while(True):
 
   my_hash = md5(text.encode()).hexdigest()
 
-  if my_hash in nice_pattern:
+  if len(set(my_hash))==1:
     print("nice_pattern was found!!!!")
     print(text,my_hash)
   if len(set(my_hash[:N_nice_match]))==1:
     print("N_nice_match of %d was found!!!!" % N_nice_match)
+    print(text,my_hash)
+  if text==my_hash:
+    print("Gold MD5 was found!!!!")
     print(text,my_hash)
   sleep(0.01)
